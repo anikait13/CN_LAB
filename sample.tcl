709af3951219ef38e74c6057e654511bd80c01b5
anikait <anikaitth@gmail.com>
2022-11-01 09:51:12 +0530

    LAN

--- sample.tcl
+++ sample.tcl
@@ -1,6 +1,10 @@
 # create a simulator object
 set ns [new Simulator]
 
+# define different colors for nam data flows
+$ns color 1 green
+$ns color 2 red
+
 # open the nam trace file
 set nf [open out.nam w]
 $ns namtrace-all $nf
@@ -16,33 +20,29 @@ proc finish {} {
 
 
 # create 4 nodes
-set n0 [$ns node]
-set n1 [$ns node]
-set n2 [$ns node]
-set n3 [$ns node]
-
-# create links between the nodes
-$ns duplex-link $n0 $n2 2Mb 10ms DropTail
-$ns duplex-link $n1 $n2 2Mb 10ms DropTail
-$ns duplex-link $n2 $n3 1.7Mb 20ms DropTail
+set A [$ns node]
+set B [$ns node]
+set C [$ns node]
+set D [$ns node]
+set E [$ns node]
 
 # for LAN set lan0 [$ns newLan "$n0 $n1 $n2 $n3" 0.7Mb 20ms LL Queue/DropTail MAC/Csma/-802_3 channel] 
 
+
+# create links between the nodes
+$ns duplex-link $A $B 2Mb 10ms DropTail
+$ns duplex-link $B $C 2Mb 10ms DropTail
+$ns duplex-link $C $D 2Mb 10ms DropTail
+$ns duplex-link $D $E 2Mb 10ms DropTail
+
+
+
 # UDP traffic source
 # create a UDP agent and attach it to node node 1
-set udp [new Agent/UDP]
-$ns attach-agent $n1 $udp
-
-# create a TCP agent and attach it to node node 0
 set tcp [new Agent/TCP]
-$ns attach-agent $n0 $tcp
-
-
-# create a CBR traffic source and attach it to udp
-set cbr [new Application/Traffic/CBR]
-$cbr set packetSize_ 500
-$cbr set interval_ 0.005
-$cbr attach-agent $udp
+$ns attach-agent $A $tcp
+$tcp set fid_ 1		# green color
+$tcp set class_ 1
 
 # create a FTP traffic source and attach it to tcp
 set ftp [new Application/FTP]
@@ -51,31 +51,27 @@ $ftp attach-agent $tcp
 
 # creat a Null agent (a traffic sink) and at
 set null [new Agent/Null]
-$ns attach-agent $n3 $null
-$ns connect $udp $null
-
+$ns attach-agent $E $null
+$ns connect $tcp $null
 
 # creat a Sink agent (a traffic sink) and at
 set sink [new Agent/TCPSink]
-$ns attach-agent $n3 $sink
+$ns attach-agent $E $sink
 $ns connect $tcp $sink
 
 
 # schedule events for all the flows
-$ns at 0.1 "$cbr start"
 $ns at 1.0 "$ftp start"
-$ns at 4.0 "$ftp stop"
-$ns at 4.5 "$cbr stop"
-
+$ns at 3.0 "$ftp stop"
 
 # call the finish procedure after 6 seconds of simulation time
 $ns at 5.0 "finish"
 
-# Print CBR packet size and interval
-puts "CBR packet size = [$cbr set packet_size_]"
+
 
 # run the simulation
 $ns run
+
 
 
 
